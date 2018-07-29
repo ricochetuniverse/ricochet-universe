d4bcf46d70bbed84dfb48cc99b5a246cd396233f
Ng Yik Phang <ngyikp@gmail.com>
2018-07-29 12:49:58 +0800

    Add force download catalog feature

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,19 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Services\CatalogService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class CatalogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $response = Cache::remember('level_catalog', 10, function () {
+        $catalog = Cache::remember('level_catalog', 10, function () {
             $catalogService = new CatalogService;
             return $catalogService->getCatalog();
         });
 
-        return response($response)
+        $response = response($catalog)
             ->setCache(['public' => true, 'max_age' => 60 * 10])
             ->header('Content-Type', 'text/plain');
+        
+        if ($request->input('download')) {
+            $response->header(
+                'Content-Disposition',
+                $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'catalogx.bin')
+            );
+        }
+
+        return $response;
     }
 }
