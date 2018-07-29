b1024ccec56b886c739783c695ebe571d9a7e47b
Ng Yik Phang <ngyikp@gmail.com>
2018-07-29 18:15:29 +0800

    Add custom page not found screen

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="card-header">Page not found</div>
+
+                    <div class="card-body">
+                        Sorry, the page you are looking for could not be found.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
