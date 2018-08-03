e0df76b8e9a3d2c6ec35634bc876a04a457dbf2d
Ng Yik Phang <ngyikp@gmail.com>
2018-08-03 16:05:42 +0800

    Include Preact debug/devtools on development
    React Devtools doesn’t seem to work though o_O

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,13 @@ import '../sass/app.scss';
 import $ from 'jquery';
 import 'bootstrap';
 
+// Bootstrap
 $('[data-toggle="tooltip"]').tooltip();
 
+// Preact
+if (process.env.NODE_ENV === 'development') {
+    require('preact/debug');
+}
+
+// Website stuff
 import './round-info';
