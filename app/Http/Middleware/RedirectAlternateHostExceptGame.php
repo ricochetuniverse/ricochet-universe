e1df4fd5fe55db7e80699802af6d16d7ac22c39f
Ng Yik Phang <ngyikp@gmail.com>
2018-08-05 17:42:07 +0800

    Redirect alternate host to main domain
    This doesn’t happen for user-agents that begin with “Ricochet Infinity”
    
    Yes, you can use the hosts file to redirect the catalog too, it’ll work :)

---
+++ app/Http/Middleware/RedirectAlternateHostExceptGame.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RedirectAlternateHostExceptGame
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (strpos($request->header('User-Agent'), 'Ricochet Infinity ') !== 0) {
+            if ($request->root() === 'http://www.ricochetinfinity.com') {
+                return redirect($this->buildUrl($request->getBaseUrl()));
+            }
+        }
+
+        return $next($request);
+    }
+
+    private function buildUrl($fullUrl)
+    {
+        $parsedUrl = parse_url($fullUrl);
+        $parsedUrl['host'] = config('app.url');
+        $finalUrl = $parsedUrl['host'];
+        $finalUrl .= $parsedUrl['path'];
+        $finalUrl .= isset($parsedUrl['query']) ? '?' . $parsedUrl['query'] : '';
+
+        return $finalUrl;
+    }
+}
