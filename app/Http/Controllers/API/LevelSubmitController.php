fb22f3a01d2c3c7b38dc300f4dc41deaaac823a4
Ng Yik Phang <ngyikp@gmail.com>
2018-08-05 12:59:30 +0800

    Disable level submission until it is done

--- app/Http/Controllers/API/LevelSubmitController.php
+++ app/Http/Controllers/API/LevelSubmitController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LevelSubmitController extends Controller
 {
     public function submit(Request $request)
     {
+        if (!app()->environment('local')) {
+            throw new NotFoundHttpException;
+        }
+
         $file = $request->file('file');
 
         // todo use $file->originalName, must sanitize
