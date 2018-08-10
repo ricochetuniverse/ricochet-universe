733e4370420ab7972e54656f8bc761879269dc56
Ng Yik Phang <ngyikp@gmail.com>
2018-08-11 02:59:05 +0800

    Fix missing collapse JS from Bootstrap
    It’s required for navbar expand/collapse on mobile

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,6 +3,7 @@ import '../sass/app.scss';
 import $ from 'jquery';
 
 // Bootstrap
+import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/modal';
 import 'bootstrap/js/dist/tooltip';
