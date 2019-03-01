4f9a80479ac41dc87cc017408519ec751d97a4de
Ng Yik Phang <ngyikp@gmail.com>
2019-03-01 22:58:22 +0800

    Improve cookie security with Secure and SameSite

--- config/session.php
+++ config/session.php
@@ -124,10 +124,7 @@ return [
     |
     */
 
-    'cookie' => env(
-        'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
-    ),
+    'cookie' => env('SESSION_COOKIE', 'ricochet_universe_session'),
 
     /*
     |--------------------------------------------------------------------------
@@ -194,6 +191,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => 'lax',
 
 ];
