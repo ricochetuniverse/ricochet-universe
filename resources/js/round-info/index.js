d66baffd04545a1ad3053436923f84a44600a701
Ng Yik Phang <ngyikp@gmail.com>
2019-04-10 22:35:04 +0800

    Rewrite round info details page without jQuery
    Preparing for future Bootstrap without jQuery

--- resources/js/round-info/index.js
+++ resources/js/round-info/index.js
@@ -1,35 +1,37 @@
-import $ from 'jquery';
 import {h, render} from 'preact';
 
 import RoundInfoModal from './RoundInfoModal';
 
 let modalWrap;
 
-$('.roundInfo__link').on('click', function(ev) {
-    ev.preventDefault();
+const links = document.getElementsByClassName('roundInfo__link');
+for (let i = 0, len = links.length; i < len; i += 1) {
+    links[i].addEventListener('click', (ev) => {
+        ev.preventDefault();
 
-    const $link = $(this);
+        const link = ev.currentTarget;
 
-    const props = {
-        name: $link.data('round-name'),
-        author: $link.data('round-author'),
-        note1: $link.data('round-note-1'),
-        note2: $link.data('round-note-2'),
-        note3: $link.data('round-note-3'),
-        note4: $link.data('round-note-4'),
-        note5: $link.data('round-note-5'),
-        source: $link.data('round-source'),
-        imageUrl: $link.data('round-image-url'),
-    };
+        const props = {
+            name: link.dataset['roundName'],
+            author: link.dataset['roundAuthor'],
+            note1: link.dataset['roundNote-1'],
+            note2: link.dataset['roundNote-2'],
+            note3: link.dataset['roundNote-3'],
+            note4: link.dataset['roundNote-4'],
+            note5: link.dataset['roundNote-5'],
+            source: link.dataset['roundSource'],
+            imageUrl: link.dataset['roundImageUrl'],
+        };
 
-    if (!modalWrap) {
-        modalWrap = document.createElement('div');
-        document.body.appendChild(modalWrap);
-    }
+        if (!modalWrap) {
+            modalWrap = document.createElement('div');
+            document.body.appendChild(modalWrap);
+        }
 
-    render(
-        <RoundInfoModal {...props} />,
-        modalWrap,
-        modalWrap.lastElementChild
-    );
-});
+        render(
+            <RoundInfoModal {...props} />,
+            modalWrap,
+            modalWrap.lastElementChild
+        );
+    });
+}
