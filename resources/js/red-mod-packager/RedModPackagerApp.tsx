2c8fece99f3e0ddb4de8cccf1dd5ec49a9f3fa8b
Ng Yik Phang <ngyikp@gmail.com>
2025-06-17 17:36:08 +0100

    Convert RED mod packager to use hooks 🪝

--- resources/js/red-mod-packager/RedModPackagerApp.tsx
+++ resources/js/red-mod-packager/RedModPackagerApp.tsx
@@ -1,4 +1,4 @@
-import {Component, createRef} from 'preact';
+import {useCallback, useEffect, useRef, useState} from 'preact/hooks';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -8,15 +8,6 @@ import CustomFileInput from '../CustomFileInput';
 
 import generateZip, {type FileWithPath} from './generate-zip';
 
-type State = Readonly<{
-    folderName: string;
-    error: string;
-
-    packageTime: Date | null;
-    downloadButtonUrl: string;
-    files: FileWithPath[];
-}>;
-
 function getAssumedDirectoryPrefix(file: File) {
     const path = file.webkitRelativePath;
     const split = path.split('/');
@@ -24,197 +15,187 @@ function getAssumedDirectoryPrefix(file: File) {
     return split[0] + '/';
 }
 
-export default class RedModPackagerApp extends Component<{}, State> {
-    state: State = {
-        folderName: '',
-        error: '',
+export default function RedModPackagerApp() {
+    const [folderName, setFolderName] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
-        packageTime: null,
-        downloadButtonUrl: '',
-        files: [],
-    };
+    const [packageTime, setPackageTime] = useState<Date | null>(null);
+    const [downloadButtonUrl, setDownloadButtonUrl] = useState<string | null>(
+        null
+    );
+    const [files, setFiles] = useState<FileWithPath[]>([]);
 
-    fileInputRef = createRef<HTMLInputElement>();
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
-    render() {
-        return (
-            <div className="mb-n3">
-                <Card className="mb-3">
-                    <Card.Header>RED mod packager</Card.Header>
+    const onFileChange = useCallback(async function (fileInputEvent: Event) {
+        reset();
 
-                    <Card.Body>
-                        <p>
-                            Select your mod’s folder to package it to a{' '}
-                            <code>.RED</code> file.
-                        </p>
+        try {
+            const fileInput = fileInputEvent.target;
+            if (!(fileInput instanceof HTMLInputElement)) {
+                throw new Error('Expected HTMLInputElement');
+            }
 
-                        <p>
-                            The folder name will be automatically used as the
-                            mod’s name.
-                        </p>
+            const files = fileInput.files;
+            if (!files || files.length === 0) {
+                return;
+            }
 
-                        <div className="d-flex">
-                            <CustomFileInput
-                                directory={true}
-                                ref={this.fileInputRef}
-                            />
+            // Get the first file's directory prefix, and use it for all
+            const directoryPrefix = getAssumedDirectoryPrefix(files[0]);
 
-                            <Button
-                                variant="outline-primary"
-                                disabled={!this.state.downloadButtonUrl}
-                                className="ms-2"
-                                onClick={this.resetButtonClicked}
-                            >
-                                Reset
-                            </Button>
-                        </div>
-                    </Card.Body>
-                </Card>
+            // Sanity checking...
+            for (let i = 0, len = files.length; i < len; i += 1) {
+                const file = files[i];
+                const path = file.webkitRelativePath;
 
-                {this.state.error ? (
-                    <Alert variant="danger">{this.state.error}</Alert>
-                ) : null}
-
-                {this.state.packageTime && this.state.downloadButtonUrl ? (
-                    <Card className="mb-3">
-                        <Card.Header>Package ready</Card.Header>
-
-                        <Card.Body>
-                            <p>
-                                Packaged the{' '}
-                                <code>{this.state.folderName}</code> folder on{' '}
-                                {this.state.packageTime.toString()}, reset this
-                                tool to re-package any new or modified files.
-                            </p>
-
-                            <p>
-                                Be sure to test your <code>.RED</code> file on a
-                                clean copy of the game, there might be
-                                differences compared to using the{' '}
-                                <code>Data</code> folder.
-                            </p>
-
-                            <Button
-                                as="a"
-                                href={this.state.downloadButtonUrl}
-                                download={this.state.folderName + '.red'}
-                                variant="outline-primary"
-                            >
-                                Download {this.state.folderName + '.red'}
-                            </Button>
-                        </Card.Body>
-                    </Card>
-                ) : null}
-
-                {this.state.files.length > 0 ? (
-                    <Card className="mb-3">
-                        <Card.Header>Packaged files</Card.Header>
-
-                        <Card.Body>
-                            <ol class="m-0">
-                                {this.state.files.map((file) => {
-                                    return <li key={file.path}>{file.path}</li>;
-                                })}
-                            </ol>
-                        </Card.Body>
-                    </Card>
-                ) : null}
-            </div>
-        );
-    }
-
-    componentDidMount() {
-        const ref = this.fileInputRef.current;
-        if (ref) {
-            uppie(ref, this.onFileChange);
-        }
-    }
-
-    componentDidUpdate(_prevProps: {}, prevState: State) {
-        if (this.state.downloadButtonUrl !== prevState.downloadButtonUrl) {
-            window.URL.revokeObjectURL(prevState.downloadButtonUrl);
-        }
-    }
-
-    onFileChange = (fileInputEvent: Event /*, formData, files*/) => {
-        this.reset(async () => {
-            try {
-                const fileInput = fileInputEvent.target;
-                if (!(fileInput instanceof HTMLInputElement)) {
-                    throw new Error('Expected HTMLInputElement');
-                }
-
-                const files = fileInput.files;
-                if (!files || files.length === 0) {
-                    return;
-                }
-
-                // Get the first file's directory prefix, and use it for all
-                const directoryPrefix = getAssumedDirectoryPrefix(files[0]);
-
-                // Sanity checking...
-                for (let i = 0, len = files.length; i < len; i += 1) {
-                    const file = files[i];
-                    const path = file.webkitRelativePath;
-
-                    if (
-                        path.substring(0, directoryPrefix.length) !==
-                        directoryPrefix
-                    ) {
-                        throw new Error(
-                            'Unexpected directory prefix, should be ' +
-                                directoryPrefix
-                        );
-                    }
-                }
-
-                this.setState({
-                    folderName: directoryPrefix.substring(
-                        0,
-                        directoryPrefix.length - 1
-                    ),
-                });
-
-                const result = await generateZip(
-                    Array.from(files),
+                if (
+                    path.substring(0, directoryPrefix.length) !==
                     directoryPrefix
-                );
-
-                this.setState({
-                    packageTime: new Date(),
-                    downloadButtonUrl: window.URL.createObjectURL(result.zip),
-                    files: result.files,
-                });
-            } catch (err) {
-                if (err instanceof Error) {
-                    this.setState({error: err.message});
-                } else {
-                    this.setState({error: err?.toString()});
+                ) {
+                    throw new Error(
+                        'Unexpected directory prefix, should be ' +
+                            directoryPrefix
+                    );
                 }
             }
-        });
-    };
 
-    reset = (callback = () => {}) => {
-        this.setState(
-            {
-                folderName: '',
-                error: '',
+            setFolderName(
+                directoryPrefix.substring(0, directoryPrefix.length - 1)
+            );
 
-                packageTime: null,
-                downloadButtonUrl: '',
-                files: [],
-            },
-            callback
-        );
-    };
+            const result = await generateZip(
+                Array.from(files),
+                directoryPrefix
+            );
 
-    resetButtonClicked = () => {
-        this.reset();
+            setPackageTime(new Date());
+            setDownloadButtonUrl(window.URL.createObjectURL(result.zip));
+            setFiles(result.files);
+        } catch (ex) {
+            console.error(ex);
 
-        const ref = this.fileInputRef.current;
+            if (ex instanceof Error) {
+                setError(ex.message);
+            } else {
+                setError(
+                    ex?.toString() ??
+                        'There was a problem packaging these files.'
+                );
+            }
+        }
+    }, []);
+
+    function reset() {
+        setFolderName(null);
+        setError(null);
+
+        setPackageTime(null);
+        setDownloadButtonUrl(null);
+        setFiles([]);
+    }
+
+    function resetButtonClicked() {
+        reset();
+
+        const ref = fileInputRef.current;
         if (ref) {
             ref.value = '';
         }
-    };
+    }
+
+    useEffect(() => {
+        const ref = fileInputRef.current;
+        if (ref) {
+            uppie(ref, onFileChange);
+        }
+    }, [onFileChange]);
+
+    const [prevDownloadButtonUrl, setPrevDownloadButtonUrl] =
+        useState(downloadButtonUrl);
+    if (
+        downloadButtonUrl !== prevDownloadButtonUrl &&
+        prevDownloadButtonUrl != null
+    ) {
+        window.URL.revokeObjectURL(prevDownloadButtonUrl);
+        setPrevDownloadButtonUrl(downloadButtonUrl);
+    }
+
+    return (
+        <div className="mb-n3">
+            <Card className="mb-3">
+                <Card.Header>RED mod packager</Card.Header>
+
+                <Card.Body>
+                    <p>
+                        Select your mod’s folder to package it to a{' '}
+                        <code>.RED</code> file.
+                    </p>
+
+                    <p>
+                        The folder name will be automatically used as the mod’s
+                        name.
+                    </p>
+
+                    <div className="d-flex">
+                        <CustomFileInput directory={true} ref={fileInputRef} />
+
+                        <Button
+                            variant="outline-primary"
+                            disabled={!downloadButtonUrl}
+                            className="ms-2"
+                            onClick={resetButtonClicked}
+                        >
+                            Reset
+                        </Button>
+                    </div>
+                </Card.Body>
+            </Card>
+
+            {error ? <Alert variant="danger">{error}</Alert> : null}
+
+            {packageTime && downloadButtonUrl ? (
+                <Card className="mb-3">
+                    <Card.Header>Package ready</Card.Header>
+
+                    <Card.Body>
+                        <p>
+                            Packaged the <code>{folderName}</code> folder on{' '}
+                            {packageTime.toString()}, reset this tool to
+                            re-package any new or modified files.
+                        </p>
+
+                        <p>
+                            Be sure to test your <code>.RED</code> file on a
+                            clean copy of the game, there might be differences
+                            compared to using the <code>Data</code> folder.
+                        </p>
+
+                        <Button
+                            as="a"
+                            href={downloadButtonUrl}
+                            download={folderName + '.red'}
+                            variant="outline-primary"
+                        >
+                            Download {folderName + '.red'}
+                        </Button>
+                    </Card.Body>
+                </Card>
+            ) : null}
+
+            {files.length > 0 ? (
+                <Card className="mb-3">
+                    <Card.Header>Packaged files</Card.Header>
+
+                    <Card.Body>
+                        <ol class="m-0">
+                            {files.map((file) => {
+                                return <li key={file.path}>{file.path}</li>;
+                            })}
+                        </ol>
+                    </Card.Body>
+                </Card>
+            ) : null}
+        </div>
+    );
 }
