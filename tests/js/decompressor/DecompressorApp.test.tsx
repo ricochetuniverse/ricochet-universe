324c431268dab5f0f2e493a1854d923963ed5c50
Ng Yik Phang <ngyikp@gmail.com>
2025-06-18 13:06:08 +0100

    Fix failing test 🙈

--- tests/js/decompressor/DecompressorApp.test.tsx
+++ tests/js/decompressor/DecompressorApp.test.tsx
@@ -4,7 +4,7 @@ import {render, screen} from '@testing-library/preact';
 import DecompressorApp from '../../../resources/js/decompressor/DecompressorApp';
 
 test('renders the app', async () => {
-    render(<DecompressorApp />);
+    render(<DecompressorApp dotnetLoaderUrl="" />);
 
     expect(await screen.findByText('Decompressor')).not.toBeNull();
 });
