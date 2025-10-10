806491060953dca5dbccc9ae5ae6a6427a0bfda5
Ng Yik Phang <ngyikp@gmail.com>
2025-10-10 12:54:48 +0100

    Update npm/PHP dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig(
     jsxA11y.flatConfigs.recommended,
     eslintReact.configs['recommended-typescript'],
     // @ts-expect-error typescript being weird and keep erroring here
-    reactHooks.configs['recommended'],
+    reactHooks.configs.flat.recommended,
     regexp.configs['flat/recommended'],
     {
         rules: {
