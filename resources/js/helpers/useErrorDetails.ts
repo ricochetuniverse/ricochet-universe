62158774479d5934f724febcd7f5a6656d81099a
Ng Yik Phang <ngyikp@gmail.com>
2026-01-03 00:27:03 +0000

    Update npm dependencies (part 2)

--- resources/js/helpers/useErrorDetails.ts
+++ resources/js/helpers/useErrorDetails.ts
@@ -1,7 +1,6 @@
 import {useState} from 'preact/hooks';
 
 export default function useErrorDetails() {
-    // eslint-disable-next-line @eslint-react/naming-convention/use-state
     return useState<
         | {
               isError: false;
