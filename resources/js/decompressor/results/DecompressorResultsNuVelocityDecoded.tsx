ffd0e5318a9dcdf5259c66b16cda8e9b9ae26dc5
Ng Yik Phang <ngyikp@gmail.com>
2026-06-15 18:52:37 +0100

    Fix missing Appearance dropdown on decompressor

--- resources/js/decompressor/results/DecompressorResultsNuVelocityDecoded.tsx
+++ resources/js/decompressor/results/DecompressorResultsNuVelocityDecoded.tsx
@@ -23,127 +23,142 @@ export default function DecompressorResultsNuVelocityDecoded({
     const [showAll, setShowAll] = useState(false);
     const [appearance, setAppearance] = useState<Appearance>('CHECKERBOARD');
 
-    return decodedImages.length > 1 ? (
+    return (
         <Stack gap={3}>
-            <Form.Check
-                type="checkbox"
-                id="decompressor-showAll"
-                checked={showAll}
-                onChange={(ev) => {
-                    setShowAll(ev.currentTarget.checked);
-                }}
-                label={`Show all ${decodedImages.length.toString()} images`}
-            />
+            {decodedImages.length > 1 ? (
+                <Form.Check
+                    type="checkbox"
+                    id="decompressor-showAll"
+                    checked={showAll}
+                    onChange={(ev) => {
+                        setShowAll(ev.currentTarget.checked);
+                    }}
+                    label={`Show all ${decodedImages.length.toString()} images`}
+                />
+            ) : null}
 
             <DecompressorImageAppearance
                 onChange={setAppearance}
                 value={appearance}
             />
 
-            {!showAll ? (
-                <>
-                    <div>
-                        <DecompressorResultsImage
-                            appearance={appearance}
-                            src={BASE64_DATA_URI + decodedImages[currentIndex]}
-                        />
-                    </div>
-
-                    <ButtonToolbar className="align-items-center">
-                        <ButtonGroup className="me-2">
-                            <Button
-                                disabled={currentIndex === 0}
-                                onClick={() => {
-                                    setCurrentIndex(0);
-                                }}
-                                title="First"
-                                variant="outline-primary"
-                            >
-                                «
-                            </Button>
-
-                            <Button
-                                disabled={currentIndex === 0}
-                                onClick={() => {
-                                    setCurrentIndex(currentIndex - 1);
-                                }}
-                                title="Previous"
-                                variant="outline-primary"
-                            >
-                                &lsaquo;
-                            </Button>
-                        </ButtonGroup>
-
-                        <Form.Control
-                            type="number"
-                            className="w-auto me-2"
-                            value={currentIndex + 1}
-                            min="1"
-                            max={decodedImages.length}
-                            onChange={(ev) => {
-                                const number = parseInt(
-                                    ev.currentTarget.value,
-                                    10
-                                );
-                                if (
-                                    number >= 1 &&
-                                    number <= decodedImages.length
-                                ) {
-                                    setCurrentIndex(number - 1);
-                                }
-                            }}
-                        />
-                        <span className="me-2">of {decodedImages.length}</span>
-
-                        <ButtonGroup>
-                            <Button
-                                disabled={
-                                    currentIndex === decodedImages.length - 1
-                                }
-                                onClick={() => {
-                                    setCurrentIndex(currentIndex + 1);
-                                }}
-                                title="Next"
-                                variant="outline-primary"
-                            >
-                                &rsaquo;
-                            </Button>
-
-                            <Button
-                                disabled={
-                                    currentIndex === decodedImages.length - 1
-                                }
-                                onClick={() => {
-                                    setCurrentIndex(decodedImages.length - 1);
-                                }}
-                                title="Last"
-                                variant="outline-primary"
-                            >
-                                »
-                            </Button>
-                        </ButtonGroup>
-                    </ButtonToolbar>
-                </>
-            ) : (
-                <div>
-                    {decodedImages.map((img, index) => {
-                        return (
+            {decodedImages.length > 1 ? (
+                !showAll ? (
+                    <>
+                        <div>
                             <DecompressorResultsImage
                                 appearance={appearance}
-                                className="me-2 mb-2"
-                                src={BASE64_DATA_URI + img}
-                                // eslint-disable-next-line @eslint-react/no-array-index-key
-                                key={index}
+                                src={
+                                    BASE64_DATA_URI +
+                                    decodedImages[currentIndex]
+                                }
                             />
-                        );
-                    })}
+                        </div>
+
+                        <ButtonToolbar className="align-items-center">
+                            <ButtonGroup className="me-2">
+                                <Button
+                                    disabled={currentIndex === 0}
+                                    onClick={() => {
+                                        setCurrentIndex(0);
+                                    }}
+                                    title="First"
+                                    variant="outline-primary"
+                                >
+                                    «
+                                </Button>
+
+                                <Button
+                                    disabled={currentIndex === 0}
+                                    onClick={() => {
+                                        setCurrentIndex(currentIndex - 1);
+                                    }}
+                                    title="Previous"
+                                    variant="outline-primary"
+                                >
+                                    &lsaquo;
+                                </Button>
+                            </ButtonGroup>
+
+                            <Form.Control
+                                type="number"
+                                className="w-auto me-2"
+                                value={currentIndex + 1}
+                                min="1"
+                                max={decodedImages.length}
+                                onChange={(ev) => {
+                                    const number = parseInt(
+                                        ev.currentTarget.value,
+                                        10
+                                    );
+                                    if (
+                                        number >= 1 &&
+                                        number <= decodedImages.length
+                                    ) {
+                                        setCurrentIndex(number - 1);
+                                    }
+                                }}
+                            />
+                            <span className="me-2">
+                                of {decodedImages.length}
+                            </span>
+
+                            <ButtonGroup>
+                                <Button
+                                    disabled={
+                                        currentIndex ===
+                                        decodedImages.length - 1
+                                    }
+                                    onClick={() => {
+                                        setCurrentIndex(currentIndex + 1);
+                                    }}
+                                    title="Next"
+                                    variant="outline-primary"
+                                >
+                                    &rsaquo;
+                                </Button>
+
+                                <Button
+                                    disabled={
+                                        currentIndex ===
+                                        decodedImages.length - 1
+                                    }
+                                    onClick={() => {
+                                        setCurrentIndex(
+                                            decodedImages.length - 1
+                                        );
+                                    }}
+                                    title="Last"
+                                    variant="outline-primary"
+                                >
+                                    »
+                                </Button>
+                            </ButtonGroup>
+                        </ButtonToolbar>
+                    </>
+                ) : (
+                    <div>
+                        {decodedImages.map((img, index) => {
+                            return (
+                                <DecompressorResultsImage
+                                    appearance={appearance}
+                                    className="me-2 mb-2"
+                                    src={BASE64_DATA_URI + img}
+                                    // eslint-disable-next-line @eslint-react/no-array-index-key
+                                    key={index}
+                                />
+                            );
+                        })}
+                    </div>
+                )
+            ) : (
+                <div>
+                    <DecompressorResultsImage
+                        appearance={appearance}
+                        src={BASE64_DATA_URI + decodedImages[0]}
+                    />
                 </div>
             )}
         </Stack>
-    ) : (
-        <DecompressorResultsImage
-            appearance={appearance}
-            src={BASE64_DATA_URI + decodedImages[0]}
-        />
     );
 }
